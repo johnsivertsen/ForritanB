b51621350a837d660c63d1300654464c45d8079c
John Sivertsen <john@sivertsen.fo>
2020-01-15 13:11:09 +0000

    Add more buttons

--- GuiTemplate/src/fo/josi/GuiTemplate/Form.java
+++ GuiTemplate/src/fo/josi/GuiTemplate/Form.java
@@ -1,6 +1,7 @@
 package fo.josi.GuiTemplate;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,46 +9,69 @@ public class Form {
     private JFrame frame;
     private JTextField textField;
     private JTextArea textArea;
-    private JButton button;
+
+    private ActionListener button1Press;
+    private ActionListener button2Press;
+    private ActionListener button3Press;
 
     public Form() {
         frame = new JFrame();
-        frame.setSize(720, 480);
+        frame.setSize(620, 480);
         frame.setLayout(null);
 
-        button = new JButton();
         textField = new JTextField();
         textArea = new JTextArea();
     }
 
     public void Show() {
-        AddButton("Trýst her");
-        AddTextField();
-        AddTextArea();
-        frame.setVisible(true);
-    }
 
-    private void AddTextField() {
-        textField.setBounds(10, 10, 600, 25);
-        frame.add(textField);
-    }
-
-    private void AddButton(String caption) {
-        button.setText(caption);
-        button.setBounds(610, 10, 100, 25);
-        button.addActionListener(new ActionListener() {
+        button1Press = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String inputText  = textField.getText();
-                Log("Tekstur: " + inputText);
-                //String datepart = inputText.substring(0,2);
+                Log("Knapp 1: " + inputText);
             }
-        });
+        };
+
+        button2Press = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputText  = textField.getText();
+                Log("Knapp 2: " + inputText);
+            }
+        };
+
+        button3Press = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputText  = textField.getText();
+                Log("Knapp 3: " + inputText);
+            }
+        };
+
+        AddTextField(new Rectangle(10, 10, 600, 25));
+        AddButton("Næsti tími", new Rectangle(10, 35, 100, 25), button1Press);
+        AddButton("Et", new Rectangle(120, 35, 100, 25), button2Press);
+        AddButton("Gang", new Rectangle(230, 35, 100, 25), button3Press);
+        AddTextArea(new Rectangle(10, 65, 600, 400));
+        frame.setVisible(true);
+    }
+
+    private void AddTextField(Rectangle position) {
+        textField.setBounds(position);
+        frame.add(textField);
+    }
+
+    private void AddButton(String caption, Rectangle rectangle, ActionListener actionListener) {
+        JButton button = new JButton();
+        button.setText(caption);
+        button.setBounds(rectangle);
+        button.addActionListener(actionListener);
         frame.add(button);
     }
 
-    private void AddTextArea() {
-        textArea.setBounds(10, 45, 600, 400);
+    private void AddTextArea(Rectangle position) {
+        textArea.setBounds(position);
         frame.add(textArea);
     }
 
